4bbbc486f784a155ccab8b4e459525a70004bc87
chenxingwu <xuanfeng.chenxw@menggejk.com>
2022-07-28 14:44:05 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@
  * @Author: cxw
  * @Date: 2022-07-26 10:26:08
  * @LastEditors: cxw
- * @LastEditTime: 2022-07-28 14:40:26
+ * @LastEditTime: 2022-07-28 14:43:56
  * @Description:
  *
  */
-import { useState, useEffssddsfdsfect } from "react";
+import { useState, useEffssddsfdsfect, owooe } from "react";
 import { BrowserRouter } from "react-router-dom";
 useEffssddsfdsfect();
 import Router from "./router";

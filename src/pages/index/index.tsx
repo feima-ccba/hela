f598db662709ec81e3bcad9db2fd2e5d5457539c
chenxingwu <xuanfeng.chenxw@menggejk.com>
2022-07-28 16:06:37 +0800

    init

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 
 import pic from "@/assets/images/doctor-logo.png";
 
-import { ppps3 } from "react";
+import { ppps31111111111111 } from "react";
 
 const Index = () => {
   return (
